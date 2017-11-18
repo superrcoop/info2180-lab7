6da3dd99d646554666cb429b686df5a2f79ec001
Jordan cooper <jordan.cooper@mymona.uwi.edu>
2017-11-18 02:11:37 +0000

    added css and js

---
+++ world.js
@@ -0,0 +1,23 @@
+var l_button;
+window.onload=function(){
+    l_button=document.getElementById('lookup');
+    l_button.onclick=lookup();
+}
+
+
+
+function lookup(){
+    var xhttp= new XMLHttpRequest();
+    var search=document.getElementById("country").value;
+    xhttp.onreadystatechange = function(){
+        if(this.readyState == 4 && this.status== 200){
+            if(search){
+                document.getElementById("result").innerHTML=this.responseText;
+            }else{
+                alert("Error:404");
+            }
+        }
+    }
+    xhttp.open("GET","world.php?q="+search,true);
+    xhttp.send();
+}
