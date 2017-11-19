523c34cdfef348fa36a3e0ce71ac36cf456829b6
Jordan cooper <jordan.cooper@mymona.uwi.edu>
2017-11-19 05:17:32 +0000

    attempt at checkbox-all

--- world.js
+++ world.js
@@ -1,15 +1,22 @@
 var l_button;
+var all_lookup;
 window.onload=function(){
     l_button=document.getElementById('lookup');
+    all_lookup=document.getElementById('all').checked;
     l_button.onclick=function(){
         var xhttp= new XMLHttpRequest();
         var search=document.getElementById("country").value;
         xhttp.onreadystatechange = function(){
         if(this.readyState == 4 && this.status== 200){
-            if(search){
+            if(all_lookup){
+                search="all";
+                document.getElementById("result").innerHTML=this.responseText;
+                alert("fwc");
+              
+            }else if(search){
                 document.getElementById("result").innerHTML=this.responseText;
             }else{
-                alert("Error:404");
+                alert("Error");
             }
         }
     }
